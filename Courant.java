20b49720a54897ca3f3062747ee4ce3c8d3d848b
Sul <gsh@xp1-b317-03-sb.sul.org>
2013-01-10 17:42:00 +0100

    Classe Compte Eparge et Courant completed!

---
+++ Courant.java
@@ -0,0 +1,21 @@
+public class Courant extends Compte
+{
+	private double decouvert_autorise;
+	
+	public Courant(Client client, int numero, double solde, double dec_autorise) 
+	{
+		super(client, numero, solde);
+		decouvert_autorise = dec_autorise;
+	}
+	
+	@Override
+	public boolean debiter(double montant)
+	{
+		if(solde - montant >= decouvert_autorise)
+		{
+			solde -= montant;
+		}
+		return false;
+	}
+	
+}
